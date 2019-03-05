8846d513e840bdaca3245b2295f0985c3c043801
DANILO LOFOCO <d.lofoco1@studenti.uniba.it>
2019-03-05 13:39:11 +0100

    rating activity

---
+++ app/src/main/java/com/example/firebaseapp/RatingBarValue.java
@@ -0,0 +1,20 @@
+package com.example.firebaseapp;
+
+/**
+ * Created by Asus on 05/03/2019.
+ */
+
+public class RatingBarValue {
+    private Integer value1 ;
+
+    public RatingBarValue(){ //costructor default
+    }
+
+    public void setValue(int valuee) {
+        this.value1 = value1;
+    }
+
+    public Integer getValue() {
+        return value1;
+    }
+}
--- app/src/main/java/com/example/firebaseapp/ShowRating.java
+++ app/src/main/java/com/example/firebaseapp/ShowRating.java
@@ -4,20 +4,55 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RatingBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class ShowRating extends AppCompatActivity {
-    RatingBar ratingbar;
-    Button button;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class ShowRating extends AppCompatActivity {
+   // RatingBar ratingbar;
+    // Button button;
+    DatabaseReference reff;
+    RatingBar ratingRatingBar;
+    Button submitrating,btnsave;
+    TextView ratingDisplayTextView;
+    EditText ratingDisplayText;
+    RatingBarValue rating;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_rating);
+
+         ratingRatingBar= (RatingBar)findViewById(R.id.ratingBar);
+         submitrating= (Button)findViewById(R.id.buttonRating);
+         ratingDisplayTextView = (TextView)findViewById(R.id.rating_display_text_view);
+
+
+        submitrating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ratingDisplayTextView.setText("your rating is: "+ ratingRatingBar.getRating());
+
+            }
+        });
+        rating=new RatingBarValue();
+        reff= FirebaseDatabase.getInstance().getReference().child("rating");
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               int  value =Integer.parseInt(String.valueOf(ratingRatingBar.getRating()));
+               rating.setValue(value);
+                reff.push().setValue(rating);
+                //Toast toast = Toast.makeText(ShowRating.this ,"data inserted sussefully",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
-    protected void onCreate(Bundle savedInstanceState) {
+    /*protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addListenerOnButtonClick();
@@ -34,4 +69,5 @@ public class ShowRating extends AppCompatActivity {
             }
         });
     }
+    */
 }
